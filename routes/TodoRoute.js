301228b5dfbcf392ca4f172af0ae16cb01b37378
tsabitsalsabil <tsabitsalsabil@gmail.com>
2023-01-26 01:00:29 +0700

    penambahan delete

--- routes/TodoRoute.js
+++ routes/TodoRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getTodos, getTodoById, createTodo, updateTodo } from "../controller/ToDocontroller.js";
+import { getTodos, getTodoById, createTodo, updateTodo, deleteTodo } from "../controller/ToDocontroller.js";
 
 const router = express.Router();
 
@@ -7,5 +7,6 @@ router.get("/todo", getTodos);
 router.get("/todo/:id", getTodoById);
 router.post("/todo", createTodo);
 router.patch("/todo/:id", updateTodo);
+router.delete("/todo/:id", deleteTodo);
 
 export default router;
